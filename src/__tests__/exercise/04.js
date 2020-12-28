bbdd53894a47902003d2069c4a4b8b8283b047af
Javier <javier.fuentesm@hotmail.com>
2020-12-28 06:40:11 -0500

    Extra credit 2

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -6,23 +6,36 @@ import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 import Counter from '../../components/counter'
+import faker from 'faker'
 
 test('submitting the form calls onSubmit with username and password', () => {
   // let submittedData
   // const handleSubmit = data => (submittedData = data)
+
+  const buildLoginForm = () => {
+    return {
+      usernameText: faker.internet.userName(),
+      passwordText: faker.internet.password(),
+    }
+  }
   const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
 
   const username = screen.getByLabelText('Username')
   const password = screen.getByLabelText('Password')
+
+  const {usernameText, passwordText} = buildLoginForm()
   const submit = screen.getByRole('button', {name: /submit/i})
 
-  userEvent.type(username, 'lalala')
-  userEvent.type(password, 'lelele')
+  userEvent.type(username, usernameText)
+  userEvent.type(password, passwordText)
 
   userEvent.click(submit)
   // expect({username: 'lalala', password: 'lelele'}).toEqual(submittedData)
-  expect(handleSubmit).toBeCalledWith({username: 'lalala', password: 'lelele'})
+  expect(handleSubmit).toBeCalledWith({
+    username: usernameText,
+    password: passwordText,
+  })
   expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
 
