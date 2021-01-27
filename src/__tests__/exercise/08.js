dc94d7640aada34e57cc4ef613a19b83a6c444b6
Javier <javier.fuentesm@hotmail.com>
2021-01-27 07:37:33 -0600

    testing react hooks with react-hooks testing library

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,15 +2,11 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen, fireEvent, act} from '@testing-library/react'
+// import {render, screen, fireEvent, act} from '@testing-library/react'
+import {renderHook, act} from '@testing-library/react-hooks'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
-// 🐨 create a simple function component that uses the useCounter hook
-// and then exposes some UI that our test can interact with to test the
-// capabilities of this hook
-// 💰 here's how to use the hook:
-// const {count, increment, decrement} = useCounter()
 const FacekComponent = ({initialCount = 0}) => {
   const {count, increment, decrement} = useCounter({initialCount})
   return (
@@ -28,20 +24,27 @@ test('exposes the count and increment/decrement functions', () => {
   // 🐨 render the
   const initialCount = 10
   // render(<FacekComponent initialCount={initialCount} />)
-  let result
-  function TestFunction({initialCount}) {
-    result = useCounter({initialCount})
+  // let result
+  // function TestFunction({initialCount}) {
+  //   result = useCounter({initialCount})
+  //
+  //   return null
+  // }
+  //
+  // render(<TestFunction initialCount={initialCount} />)
+  const {result} = renderHook(useCounter, {initialProps: {initialCount}})
 
-    return null
-  }
+  expect(result.current.count).toBe(initialCount)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(initialCount + 1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(initialCount + 1 - 1)
 
-  render(<TestFunction initialCount={initialCount} />)
-
-  expect(result.count).toBe(initialCount)
-  act(() => result.increment())
-  expect(result.count).toBe(initialCount + 1)
-  act(() => result.decrement())
-  expect(result.count).toBe(initialCount + 1 - 1)
+  // expect(result.count).toBe(initialCount)
+  // act(() => result.increment())
+  // expect(result.count).toBe(initialCount + 1)
+  // act(() => result.decrement())
+  // expect(result.count).toBe(initialCount + 1 - 1)
 
   // expect(screen.getByRole('heading')).toHaveTextContent(
   //   `The count is ${initialCount}`,
@@ -57,6 +60,21 @@ test('exposes the count and increment/decrement functions', () => {
   // expect(screen.getByRole('heading')).toHaveTextContent(
   //   `The count is ${initialCount + 1 - 1}`,
   // )
+})
+
+test('the step can be changed', () => {
+  const step = 3
+
+  const {result, rerender} = renderHook(useCounter, {
+    initialProps: {step: step},
+  })
+
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(step)
+  rerender({step: 2})
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(1)
 })
 
 /* eslint no-unused-vars:0 */
