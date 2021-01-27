c084bb31fa2e49b2ceba0b4ac84a8aee0d52f020
Javier <javier.fuentesm@hotmail.com>
2021-01-27 06:51:20 -0600

    testing react hooks

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,7 +2,7 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, fireEvent} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
@@ -11,9 +11,40 @@ import useCounter from '../../components/use-counter'
 // capabilities of this hook
 // 💰 here's how to use the hook:
 // const {count, increment, decrement} = useCounter()
+const FacekComponent = () => {
+  const {count, increment, decrement} = useCounter()
+  return (
+    <>
+      <button onClick={increment}>Increment</button>
+      <br />
+      <button onClick={decrement}>Decrement</button>
+      <br />
+      <h1>The count is {count}</h1>
+    </>
+  )
+}
 
 test('exposes the count and increment/decrement functions', () => {
-  // 🐨 render the component
+  // 🐨 render the
+  const initialCount = 0
+  render(<FacekComponent />)
+
+  expect(screen.getByRole('heading')).toHaveTextContent(
+    `The count is ${initialCount}`,
+  )
+
+  const increment = screen.getByRole('button', {name: /increment/i})
+  fireEvent.click(increment)
+  expect(screen.getByRole('heading')).toHaveTextContent(
+    `The count is ${initialCount + 1}`,
+  )
+  const decrement = screen.getByRole('button', {name: /decrement/i})
+  fireEvent.click(decrement)
+  expect(screen.getByRole('heading')).toHaveTextContent(
+    `The count is ${initialCount + 1 - 1}`,
+  )
+  screen.debug()
+
   // 🐨 get the elements you need using screen
   // 🐨 assert on the initial state of the hook
   // 🐨 interact with the UI using userEvent and assert on the changes in the UI
